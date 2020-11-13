1d46a49f277a2e55d6c9229a7edf28ab7c79dae0
maratmuginov <mmuginov@protonmail.com>
2020-11-12 16:18:31 -0800

    Upload Decompiled Assembly

---
+++ Assembly-CSharp/RimWorld.IO/VirtualFileInfoExt.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Runtime.CompilerServices;
+
+namespace RimWorld.IO
+{
+	public static class VirtualFileInfoExt
+	{
+		public static XDocument LoadAsXDocument(this VirtualFile file)
+		{
+			using Stream input = file.CreateReadStream();
+			return XDocument.Load(XmlReader.Create(input), LoadOptions.SetLineInfo);
+		}
+	}
+}
